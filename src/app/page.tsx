6f434336b221ccb4992c3bc89b92ed78df8fbc1d
Siva K <koyyasiva083@gmail.com>
2025-10-23 22:05:04 +0530

    Normalize line endings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import React from 'react'
 import Dashboard from '@/components/Dashboard/Dashboard'
+import AnalyticsOverview from '@/components/Analytics/AnalyticsOverview'
+import PaymentDashboard from '@/components/Payment/PaymentDashboard'
 
 export default function DashboardLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Dashboard/>
+      <AnalyticsOverview />
+      <PaymentDashboard />
     </div>
   )
 }
